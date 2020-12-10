b60122758f5a869ec5281f616a7ac3af585ba5a4
Islam Umarov <Ihtyyarovich@gmail.com>
2020-12-10 22:32:18 +0300

    adding hidden atribute for better performance

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -106,11 +106,11 @@ export class EventsListComponent{
       time: '9:00 am',
       price: 950.00,
       imageUrl: '/assets/images/ng-nl.png',
-      location: {
+     /* location: {
         address: 'The NG-NL Convention Center & Scuba Shop',
         city: 'Amsterdam',
         country: 'Netherlands'
-      },
+      },*/
       sessions: [
         {
           id: 1,
