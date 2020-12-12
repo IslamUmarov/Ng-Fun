26d1c34a772f82d445bc59944bb5b4760967f3c0
Islam Umarov <Ihtyyarovich@gmail.com>
2020-12-12 18:17:39 +0300

    DI Services

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,40 +1,55 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector:'event-thumbnail',
-  template: `
-  <div>
+  selector: "event-thumbnail",
+  template: ` <div>
     <div class="well hoverwell thumbnail">
-      <h2>{{event.name}}</h2>
-      <div>Date: {{event.date}}</div>
-      <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">Time: {{event.time}} 
+      <h2>{{ event.name }}</h2>
+      <div>Date: {{ event.date }}</div>
+      <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
+        Time: {{ event.time }}
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
         <span *ngSwitchDefault>(Normal Start)</span>
       </div>
-      <div>Price: \${{event.price}}</div>
+      <div>Price: \${{ event.price }}</div>
       <div>
-        <span>Location: {{event?.location?.address}}</span>
-        <span class="pad-left"> {{event?.location?.city}}, {{event?.location?.country}}</span>
+        <span>Location: {{ event?.location?.address }}</span>
+        <span class="pad-left">
+          {{ event?.location?.city }}, {{ event?.location?.country }}</span
+        >
       </div>
     </div>
   </div>`,
-  styles:[`
-  .green {color: #003300 !important; }
-  .bold { font-weight: bold; }
-  .thumbnail { min-height: 210px; }
-  .pad-left { margin-left: 10px; }
-  .well div {
-    color: #bbb;
-  }
-  `]
+  styles: [
+    `
+      .green {
+        color: #003300 !important;
+      }
+      .bold {
+        font-weight: bold;
+      }
+      .thumbnail {
+        min-height: 210px;
+      }
+      .pad-left {
+        margin-left: 10px;
+      }
+      .well div {
+        color: #bbb;
+      }
+    `,
+  ],
 })
-export class EventThumbnailComponent{
+export class EventThumbnailComponent {
   @Input() event: any;
 
-  getStartTimeClass() {
-    if(this.event && this.event.time === "8:00 am")
-      return ['green bold'];
-    return [];
+  getStartTimeStyle(): any {
+    if (this.event && this.event.time === "8:00 am")
+      return {
+        color: "#003300",
+        "font-weight": "bold",
+      };
+    return {};
   }
 }
