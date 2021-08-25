e2fc1b61710178e3d516dc059cd19c3b4d217186
Islam <Ihtyyarovich@gmail.com>
2021-08-25 17:56:30 +0300

    added search

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core'
+import { EventEmitter, Injectable } from '@angular/core'
 import { Observable, Subject } from 'rxjs'
-import { IEvent } from './event.model';
+import { IEvent, ISession } from './event.model';
 
 @Injectable()
 export class EventService {
@@ -27,6 +27,22 @@ export class EventService {
 
     let index = EVENTS.findIndex(e => e.id === event.id);
     EVENTS[index] = event;
+  }
+
+  searchSessions(searchTerm: string) {
+    var term = searchTerm.toLowerCase();
+    var results: ISession[] = [];
+    EVENTS.forEach(event => {
+      var matchingSessions = event.sessions.filter(session => session.name.toLowerCase().indexOf(term) > -1);
+      matchingSessions = matchingSessions.map((session: any) => {
+        session.eventId = event.id;
+        return session;
+      })
+      results = results.concat(matchingSessions);
+    });
+    var emitter = new EventEmitter(true);
+    setTimeout(() => { emitter.emit(results); }, 100);
+    return emitter;
   }
 }
 
